d64cb3a2ea8b546bc6c3cda2d430eef84c8c0316
arora <aroramadhav65@gmail.com>
2022-05-23 13:59:39 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.title,}) : super(key: key);
 
   final String title;
 
