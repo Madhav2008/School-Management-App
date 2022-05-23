cfa5829ed395609cccb3cde6fbf162c64c9cf071
arora <aroramadhav65@gmail.com>
2022-05-23 16:32:21 +0530

    Update

---
+++ lib/Constants/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const black = Colors.black;
+const white = Colors.white;
+const primary = Color(0xFF075E54);
+const secondary = Color(0xFF25D366);
+const one = Color(0xFF128C7E);
+var grey = Colors.grey;
+const red = Colors.red;
+const blue = Colors.blue;
+const orange = Colors.orange;
+const green = Colors.green;
+const purple = Colors.purple;
+const pink = Colors.pink;
+const deepPurple = Colors.deepPurple;
+const transparent = Colors.transparent;
