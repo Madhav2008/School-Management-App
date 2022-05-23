d86457f86b3d85ab04bbf899f28ebb4b4c0a34a0
arora <aroramadhav65@gmail.com>
2022-05-23 17:14:32 +0530

    Update

--- lib/Screens/Register_Screen.dart
+++ lib/Screens/Register_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:school_management_app/Screens/Login_Screen.dart';
 
 class RegisterScreen extends StatefulWidget {
   RegisterScreen({Key? key}) : super(key: key);
