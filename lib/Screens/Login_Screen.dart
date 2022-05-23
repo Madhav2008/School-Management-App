83d05c01b9c0aae3d0bde36376538b2760c73280
arora <aroramadhav65@gmail.com>
2022-05-23 20:48:22 +0530

    Update

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:school_management_app/Screens/Forget_Password_Screen.dart';
+import 'package:school_management_app/Screens/Navigation_Screen.dart';
 import 'package:school_management_app/Screens/Register_Screen.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -166,7 +167,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => Navigation(),
+                            builder: (context) => NavigationScreen(),
                           ),
                         );
                         Fluttertoast.showToast(
