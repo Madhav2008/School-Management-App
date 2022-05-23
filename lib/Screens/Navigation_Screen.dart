a8c7101a80bb34c92b9468c569b33ecc83b18f3a
arora <aroramadhav65@gmail.com>
2022-05-23 20:53:43 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:connectivity/connectivity.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class NavigationScreen extends StatefulWidget {
