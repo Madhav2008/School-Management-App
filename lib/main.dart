0aa28970fa180dfaa3e18652ac0dfb9f45eca558
arora <aroramadhav65@gmail.com>
2022-05-23 14:01:00 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,6 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
