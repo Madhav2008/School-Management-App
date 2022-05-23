f464cdcf91d278a5731e578e769576a777a26dae
arora <aroramadhav65@gmail.com>
2022-05-23 16:03:07 +0530

    Update

--- lib/Screens/Register_Screen.dart
+++ lib/Screens/Register_Screen.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
