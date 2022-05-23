cb2b5dea94c3123c22c8225daad028bd66bab1b5
arora <aroramadhav65@gmail.com>
2022-05-23 14:47:46 +0530

    Update

--- lib/Screens/Splash_Screen.dart
+++ lib/Screens/Splash_Screen.dart
@@ -22,7 +22,7 @@ class _SplashScreenState extends State<SplashScreen> {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (context) => SimpleCalculator(),
+            builder: (context) => HomeScreen(),
           ),
         );
       },
