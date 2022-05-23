85d74382cf8b3ea465969eb61fb25653f90b67fd
arora <aroramadhav65@gmail.com>
2022-05-23 14:00:11 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
+    // This method is rerun every  setState is called, for instance as done
     // by the _incrementCounter method above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
