5cb5ca5ba0a800da235a2ec530561742415034c0
arora <aroramadhav65@gmail.com>
2022-05-23 15:44:31 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class NavigationScreen extends StatefulWidget {
+  NavigationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
