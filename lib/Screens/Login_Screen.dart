fd23660006f8e576b27b1d236e82007553a53302
arora <aroramadhav65@gmail.com>
2022-05-23 15:52:40 +0530

    Update

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -124,7 +124,7 @@ class _LoginScreenState extends State<LoginScreen> {
                             context,
                             MaterialPageRoute(
                               builder: (context) => ForgetScreen(),
-                            ));
+                            ),);
                       },
                     ),
                   ),
