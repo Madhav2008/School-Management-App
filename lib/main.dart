a975aca7f157ecf6af1fcea235a379803c76e808
arora <aroramadhav65@gmail.com>
2022-05-23 14:00:26 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every  setState is called, for instance as done
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
