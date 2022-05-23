279a0f8a027f1889c3c5d30a112bd8291f57346e
arora <aroramadhav65@gmail.com>
2022-05-23 13:57:47 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'School',
+      title: 'School Management',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
