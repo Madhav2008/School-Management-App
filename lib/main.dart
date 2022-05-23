3be97fd937081357bcf21096b50c2d6e732944b6
arora <aroramadhav65@gmail.com>
2022-05-23 14:57:54 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'School Management',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
