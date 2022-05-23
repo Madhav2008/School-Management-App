82d0a1b365c7ee3458f196abdb017ce0280d51d1
arora <aroramadhav65@gmail.com>
2022-05-23 16:10:31 +0530

    Update

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -162,9 +162,35 @@ class _LoginScreenState extends State<LoginScreen> {
                       child: Container(
                         width: MediaQuery.of(context).size.width * 0.5,
                         height: 50,
-                        child: Buttons(
-                          buttonText: 'Login',
+                        child: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 55,
+                          vertical: 10,
                         ),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.redAccent,
+                              Color.fromRGBO(226, 55, 68, 1.0),
+                              Color.fromRGBO(226, 55, 68, 0.80),
+                              Color.fromRGBO(226, 55, 68, 0.60),
+                            ],
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                          ),
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Send',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17.3,
+                            ),
+                          ),
+                        ),
+                      ),
                       ),
                     ),
                   ),
