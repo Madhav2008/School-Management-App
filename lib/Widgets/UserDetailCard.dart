7c4421ca68162f5140f5bc0795bffc1d1d3498d2
arora <aroramadhav65@gmail.com>
2022-05-24 19:43:25 +0530

    Update

--- lib/Widgets/UserDetailCard.dart
+++ lib/Widgets/UserDetailCard.dart
@@ -26,7 +26,7 @@ class _UserDetailCardState extends State<UserDetailCard>
           0.2,
           0.5,
           curve: Curves.fastOutSlowIn,
-        )));
+        ),));
 
     muchDelayedAnimation = Tween(begin: -1.0, end: 0.0).animate(
       CurvedAnimation(
