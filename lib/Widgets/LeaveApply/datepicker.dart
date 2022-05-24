90fde6c94e20aab7edb88f26df0020d110d9b8d8
arora <aroramadhav65@gmail.com>
2022-05-24 22:45:01 +0530

    Update

--- lib/Widgets/LeaveApply/datepicker.dart
+++ lib/Widgets/LeaveApply/datepicker.dart
@@ -32,7 +32,7 @@ class CustomDatePicker extends StatelessWidget {
       //locale: Locale('en', 'US'),
       onChanged: (String change) => onchanged,
       validator: validator,
-      onSaved: ()=>saved,
+      onSaved: (String)=>saved,
     );
   }
 }
