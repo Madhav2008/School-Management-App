87733006c3b48575de9a58814d2df32ea01b5b43
arora <aroramadhav65@gmail.com>
2022-05-24 19:40:35 +0530

    Update

--- lib/Widgets/MainDrawer.dart
+++ lib/Widgets/MainDrawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:school_management_app/Screens/Home_Screen.dart';
 import 'package:school_management_app/Widgets/DrawerListTile.dart';
 
 class MainDrawer extends StatefulWidget {
@@ -18,7 +19,7 @@ class _MainDrawerState extends State<MainDrawer> {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (BuildContext context) => Home(),
+                builder: (BuildContext context) => HomeScreen(),
               ),
             );
           },
