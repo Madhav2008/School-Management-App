281a4d390048801aa070112cd430fa982c6bb103
arora <aroramadhav65@gmail.com>
2022-05-24 23:00:38 +0530

    Update

--- lib/Screens/Attendance/TodayAttendance.dart
+++ lib/Screens/Attendance/TodayAttendance.dart
@@ -28,55 +28,55 @@ class _AttendanceState extends State<Attendance>
         },
       ),
       drawer: Drawer(
-            elevation: 0,
-            child: MainDrawer(),
-          ),
+        elevation: 0,
+        child: MainDrawer(),
+      ),
       body: SingleChildScrollView(
+        child: Column(
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            UserDetailCard(),
+            DefaultTabController(
+              length: 2, // length of tabs
+              initialIndex: 0,
               child: Column(
-            //crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              UserDetailCard(),
-              DefaultTabController(
-                length: 2, // length of tabs
-                initialIndex: 0,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 10,
-                      ),
-                      child: Container(
-                        child: TabBar(
-                          labelColor: Colors.black,
-                          unselectedLabelColor: Colors.black26,
-                          indicatorColor: Colors.black,
-                          tabs: [
-                            Tab(text: 'Today'),
-                            Tab(text: 'Overall'),
-                          ],
-                        ),
-                      ),
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
                     ),
-                    Container(
-                      height: MediaQuery.of(context).size.height*0.68, //height of TabBarView
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 10,
-                      ),
-                      child: TabBarView(
-                        children: <Widget>[
-                          TodayAttendance(),
-                          OverallAttendance(),
+                    child: Container(
+                      child: TabBar(
+                        labelColor: Colors.black,
+                        unselectedLabelColor: Colors.black26,
+                        indicatorColor: Colors.black,
+                        tabs: [
+                          Tab(text: 'Today'),
+                          Tab(text: 'Overall'),
                         ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height *
+                        0.68, //height of TabBarView
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                    ),
+                    child: TabBarView(
+                      children: <Widget>[
+                        TodayAttendance(),
+                        OverallAttendance(),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
+        ),
       ),
-      
     );
   }
 }
