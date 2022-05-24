9c84f30ac55306f9c6a196ed946eed9565a0b31e
arora <aroramadhav65@gmail.com>
2022-05-24 23:05:20 +0530

    Update

--- lib/Screens/Attendance/OverallAttendance.dart
+++ lib/Screens/Attendance/OverallAttendance.dart
@@ -10,74 +10,76 @@ class _OverallAttendanceState extends State<OverallAttendance> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [
-        OverallAttendanceCard(
-          date: "15.12.2020",
-          day: "sunday",
-          firsthalf: true,
-          secondhalf: false,
-        ),
-         OverallAttendanceCard(
-          date: "15.12.2020",
-          day: "sunday",
-          firsthalf: true,
-          secondhalf: false,
-        ),
-         OverallAttendanceCard(
-          date: "15.12.2020",
-          day: "sunday",
-          firsthalf: true,
-          secondhalf: false,
-        ),
-        OverallAttendanceCard(
-          date: "15.12.2020",
-          day: "sunday",
-          firsthalf: true,
-          secondhalf: false,
-        ),
-        OverallAttendanceCard(
-          date: "15.12.2020",
-          day: "sunday",
-          firsthalf: true,
-          secondhalf: false,
-        ),
-        OverallAttendanceCard(
-          date: "15.12.2020",
-          day: "sunday",
-          firsthalf: true,
-          secondhalf: false,
-        ),
-        OverallAttendanceCard(
-          date: "15.12.2020",
-          day: "sunday",
-          firsthalf: true,
-          secondhalf: false,
-        ),
-        OverallAttendanceCard(
-          date: "15.12.2020",
-          day: "sunday",
-          firsthalf: true,
-          secondhalf: false,
-        ),
-        OverallAttendanceCard(
-          date: "15.12.2020",
-          day: "sunday",
-          firsthalf: true,
-          secondhalf: false,
-        ),
-        OverallAttendanceCard(
-          date: "15.12.2020",
-          day: "sunday",
-          firsthalf: true,
-          secondhalf: false,
-        ),
-        OverallAttendanceCard(
-          date: "15.12.2020",
-          day: "sunday",
-          firsthalf: true,
-          secondhalf: false,
-        ),
-      ],),
+      body: ListView(
+        children: [
+          OverallAttendanceCard(
+            date: "15.12.2020",
+            day: "sunday",
+            firsthalf: true,
+            secondhalf: false,
+          ),
+          OverallAttendanceCard(
+            date: "15.12.2020",
+            day: "sunday",
+            firsthalf: true,
+            secondhalf: false,
+          ),
+          OverallAttendanceCard(
+            date: "15.12.2020",
+            day: "sunday",
+            firsthalf: true,
+            secondhalf: false,
+          ),
+          OverallAttendanceCard(
+            date: "15.12.2020",
+            day: "sunday",
+            firsthalf: true,
+            secondhalf: false,
+          ),
+          OverallAttendanceCard(
+            date: "15.12.2020",
+            day: "sunday",
+            firsthalf: true,
+            secondhalf: false,
+          ),
+          OverallAttendanceCard(
+            date: "15.12.2020",
+            day: "sunday",
+            firsthalf: true,
+            secondhalf: false,
+          ),
+          OverallAttendanceCard(
+            date: "15.12.2020",
+            day: "sunday",
+            firsthalf: true,
+            secondhalf: false,
+          ),
+          OverallAttendanceCard(
+            date: "15.12.2020",
+            day: "sunday",
+            firsthalf: true,
+            secondhalf: false,
+          ),
+          OverallAttendanceCard(
+            date: "15.12.2020",
+            day: "sunday",
+            firsthalf: true,
+            secondhalf: false,
+          ),
+          OverallAttendanceCard(
+            date: "15.12.2020",
+            day: "sunday",
+            firsthalf: true,
+            secondhalf: false,
+          ),
+          OverallAttendanceCard(
+            date: "15.12.2020",
+            day: "sunday",
+            firsthalf: true,
+            secondhalf: false,
+          ),
+        ],
+      ),
     );
   }
 }
