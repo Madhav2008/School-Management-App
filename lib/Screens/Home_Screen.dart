2784dded1589dc34a471ccc3e4b2707a066f5c9a
arora <aroramadhav65@gmail.com>
2022-05-24 22:53:07 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -115,7 +115,7 @@ class _HomeScreenState extends State<HomeScreen>
                       children: [
                         Transform(
                           transform: Matrix4.translationValues(
-                              muchDelayedAnimation.value * width, 0, 0),
+                              muchDelayedAnimation.value * width, 0, 0,),
                           child: Bouncing(
                             onPress: () {
                               Navigator.push(
