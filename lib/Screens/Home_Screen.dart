b3c6645921d3936cde3679f861a26594c09a0ac1
arora <aroramadhav65@gmail.com>
2022-05-24 22:54:28 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -193,7 +193,10 @@ class _HomeScreenState extends State<HomeScreen>
                         ),
                         Transform(
                           transform: Matrix4.translationValues(
-                              delayedAnimation.value * width, 0, 0,),
+                            delayedAnimation.value * width,
+                            0,
+                            0,
+                          ),
                           child: Bouncing(
                             onPress: () {},
                             child: DashboardCard(
