39f9e66bcbc4a7c042b3833bf7faad9a9dbfef13
arora <aroramadhav65@gmail.com>
2022-05-24 23:04:30 +0530

    Update

--- lib/Screens/Attendance/Attendance.dart
+++ lib/Screens/Attendance/Attendance.dart
@@ -5,6 +5,7 @@ import 'package:school_management/Screens/home.dart';
 import 'package:school_management/Widgets/AppBar.dart';
 import 'package:school_management/Widgets/MainDrawer.dart';
 import 'package:school_management/Widgets/UserDetailCard.dart';
+import 'package:school_management_app/Widgets/AppBar.dart';
 import 'package:school_management_app/Widgets/MainDrawer.dart';
 import 'package:school_management_app/Widgets/UserDetailCard.dart';
 
