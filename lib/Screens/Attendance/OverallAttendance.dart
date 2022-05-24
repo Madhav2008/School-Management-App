f76f18ae20a8cd5f39d1f846d6733298055de079
arora <aroramadhav65@gmail.com>
2022-05-24 23:05:34 +0530

    Update

--- lib/Screens/Attendance/OverallAttendance.dart
+++ lib/Screens/Attendance/OverallAttendance.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:school_management_app/Widgets/Attendance/OverAllAttendanceCard.dart';
 
 class OverallAttendance extends StatefulWidget {
   @override
