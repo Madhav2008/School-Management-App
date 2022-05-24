8f0cd0deb2414d9bfa62b4d11c0c45d69af35bc5
arora <aroramadhav65@gmail.com>
2022-05-24 22:39:38 +0530

    Update

--- lib/Widgets/LeaveApply/LeaveHistoryCard.dart
+++ lib/Widgets/LeaveApply/LeaveHistoryCard.dart
@@ -4,10 +4,17 @@ class LeaveHistoryCard extends StatelessWidget {
   final String status;
   final String adate;
   final String startdate;
-    final String enddate;
+  final String enddate;
   final String reason;
 
-  const LeaveHistoryCard({Key key, this.status, this.adate, this.startdate, this.enddate, this.reason}) : super(key: key);
+  const LeaveHistoryCard(
+      {Key key,
+      this.status,
+      this.adate,
+      this.startdate,
+      this.enddate,
+      this.reason})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
