93ba239742ff84e4bd953d051e2d8a39ecd97d3c
arora <aroramadhav65@gmail.com>
2022-05-24 23:08:46 +0530

    Update

--- lib/Screens/Exam/Exam_Result.dart
+++ lib/Screens/Exam/Exam_Result.dart
@@ -2,11 +2,6 @@ import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:randomizer/randomizer.dart';
 
-import 'package:school_management/Widgets/AppBar.dart';
-import 'package:school_management/Widgets/BouncingButton.dart';
-import 'package:school_management/Widgets/Exams/SubjectCard.dart';
-import 'package:school_management/Widgets/MainDrawer.dart';
-
 class ExamResult extends StatefulWidget {
   @override
   _ExamResultState createState() => _ExamResultState();
