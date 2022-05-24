7150dc3e3fc3430bf21df155e4268592730778c8
arora <aroramadhav65@gmail.com>
2022-05-24 22:50:55 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -25,10 +25,12 @@ class _HomeScreenState extends State<HomeScreen>
     );
     animationController =
         AnimationController(duration: Duration(seconds: 3), vsync: this);
-    animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
-      parent: animationController,
-      curve: Curves.fastOutSlowIn,
-    ),);
+    animation = Tween(begin: -1.0, end: 0.0).animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Curves.fastOutSlowIn,
+      ),
+    );
 
     delayedAnimation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
         parent: animationController,
