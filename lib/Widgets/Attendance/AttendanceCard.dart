bc4a297f2cebd8ac2f6594024a9298bcf0ab17d4
arora <aroramadhav65@gmail.com>
2022-05-24 22:33:41 +0530

    Update

--- lib/Widgets/Attendance/AttendanceCard.dart
+++ lib/Widgets/Attendance/AttendanceCard.dart
@@ -36,7 +36,7 @@ class _AttendanceCardState extends State<AttendanceCard>
     animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
       parent: animationController,
       curve: Curves.fastOutSlowIn,
-    ));
+    ),);
 
     delayedAnimation = Tween(begin: 1.0, end: 0.0).animate(
       CurvedAnimation(
