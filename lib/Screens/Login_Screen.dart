71d728cbb42937f7155c7d38d219cc4098e2e23e
arora <aroramadhav65@gmail.com>
2022-05-24 19:15:11 +0530

    Update

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -159,7 +159,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                   Center(
                     child: GestureDetector(
-                      onTap: () {
+                      onTap: () async {
                         // authService
                         //     .signInWithEmailAndPassword(
                         //         emailController.text, passwordController.text)
