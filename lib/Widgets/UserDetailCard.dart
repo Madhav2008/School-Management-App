0d42f4449414491b26952faded5b6ec341e36279
arora <aroramadhav65@gmail.com>
2022-05-24 19:43:12 +0530

    Update

--- lib/Widgets/UserDetailCard.dart
+++ lib/Widgets/UserDetailCard.dart
@@ -24,14 +24,16 @@ class _UserDetailCardState extends State<UserDetailCard>
         parent: animationController,
         curve: Interval(0.2, 0.5, curve: Curves.fastOutSlowIn)));
 
-    muchDelayedAnimation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
-      parent: animationController,
-      curve: Interval(
-        0.3,
-        0.5,
-        curve: Curves.fastOutSlowIn,
+    muchDelayedAnimation = Tween(begin: -1.0, end: 0.0).animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Interval(
+          0.3,
+          0.5,
+          curve: Curves.fastOutSlowIn,
+        ),
       ),
-    ),);
+    );
   }
 
   @override
