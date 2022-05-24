a82ec3936ea849a24a73c7686155dea69a45c84a
arora <aroramadhav65@gmail.com>
2022-05-24 19:39:23 +0530

    Update

--- lib/Widgets/MainDrawer.dart
+++ lib/Widgets/MainDrawer.dart
@@ -64,7 +64,10 @@ class _MainDrawerState extends State<MainDrawer> {
           name: "Library",
           ontap: () {},
         ),
-        DrawerListTile(imgpath: "downloads.png", name: "Downloads",),
+        DrawerListTile(
+          imgpath: "downloads.png",
+          name: "Downloads",
+        ),
         DrawerListTile(
           imgpath: "bus.png",
           name: "Track ",
