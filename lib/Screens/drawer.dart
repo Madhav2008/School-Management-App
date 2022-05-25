f33b927b138e8e0ad029b47e3856fc43eb2de408
arora <aroramadhav65@gmail.com>
2022-05-25 22:54:30 +0530

    Update

--- lib/Screens/drawer.dart
+++ lib/Screens/drawer.dart
@@ -72,7 +72,7 @@ class NavigationDrawerWidget extends StatelessWidget {
               ),
             ),
             GestureDetector(
-              onTap: (){},
+              onTap: () {},
               child: DrawerListTile(
                 imgpath: "../assets/images/fee.png",
                 name: "Fees",
