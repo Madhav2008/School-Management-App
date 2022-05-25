001063b76ecac0ac243b465776efd17a2eed66b8
arora <aroramadhav65@gmail.com>
2022-05-25 22:37:26 +0530

    Update

--- lib/Screens/Exam/Exam_Result.dart
+++ lib/Screens/Exam/Exam_Result.dart
@@ -24,7 +24,9 @@ class _ExamResultState extends State<ExamResult>
     animationController =
         AnimationController(duration: Duration(seconds: 3), vsync: this);
     animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
-        parent: animationController, curve: Curves.fastOutSlowIn,));
+      parent: animationController,
+      curve: Curves.fastOutSlowIn,
+    ));
 
     delayedAnimation = Tween(begin: 1.0, end: 0.0).animate(
       CurvedAnimation(
