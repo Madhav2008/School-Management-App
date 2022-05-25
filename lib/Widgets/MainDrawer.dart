56d50412bf47f7e9a2dd368bb53f62bc0bfbba1f
arora <aroramadhav65@gmail.com>
2022-05-25 17:11:39 +0530

    Update

--- lib/Widgets/MainDrawer.dart
+++ lib/Widgets/MainDrawer.dart
@@ -13,8 +13,6 @@ class MainDrawer extends StatefulWidget {
 class _MainDrawerState extends State<MainDrawer> {
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<ScaffoldState> _scaffoldKey =
-            GlobalKey<ScaffoldState>();
     return ListView(
       children: [
         DrawerListTile(
