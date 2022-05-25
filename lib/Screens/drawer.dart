0fee17cca4c878d3a4dba83c9212f475b0b392fa
arora <aroramadhav65@gmail.com>
2022-05-25 17:53:01 +0530

    Update

--- lib/Screens/drawer.dart
+++ lib/Screens/drawer.dart
@@ -9,102 +9,102 @@ class NavigationDrawerWidget extends StatelessWidget {
       child: Material(
         // color: Colors.red,
         child: ListView(
-      children: [
-        DrawerListTile(
-          imgpath: "../assets/images/home.png",
-          name: "Home",
-          ontap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (BuildContext context) => HomeScreen(),
-              ),
-            );
-          },
+          children: [
+            DrawerListTile(
+              imgpath: "../assets/images/home.png",
+              name: "Home",
+              ontap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => HomeScreen(),
+                  ),
+                );
+              },
+            ),
+            DrawerListTile(
+              imgpath: "../assets/images/attendance.png",
+              name: "Attendance",
+              ontap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => Attendance(),
+                  ),
+                );
+              },
+            ),
+            DrawerListTile(
+              imgpath: "../assets/images/classroom.png",
+              name: "Class work",
+              ontap: () {},
+            ),
+            DrawerListTile(
+              imgpath: "../assets/images/profile.png",
+              name: "Profile",
+              ontap: () {},
+            ),
+            DrawerListTile(
+              imgpath: "../assets/images/exam.png",
+              name: "Examination",
+              ontap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => ExamResult(),
+                  ),
+                );
+              },
+            ),
+            DrawerListTile(
+              imgpath: "../assets/images/fee.png",
+              name: "Fees",
+              ontap: () {},
+            ),
+            DrawerListTile(
+              imgpath: "../assets/images/calendar.png",
+              name: "Time Table",
+              ontap: () {},
+            ),
+            DrawerListTile(
+              imgpath: "../assets/images/library.png",
+              name: "Library",
+              ontap: () {},
+            ),
+            DrawerListTile(
+              imgpath: "../assets/images/downloads.png",
+              name: "Downloads",
+              ontap: () {},
+            ),
+            DrawerListTile(
+              imgpath: "../assets/images/bus.png",
+              name: "Track ",
+              ontap: () {},
+            ),
+            DrawerListTile(
+              imgpath: "../assets/images/leave_apply.png",
+              name: "Leave apply",
+              ontap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => LeaveApply(),
+                  ),
+                );
+              },
+            ),
+            DrawerListTile(
+              imgpath: "../assets/images/activity.png",
+              name: "Activity",
+              ontap: () {},
+            ),
+            DrawerListTile(
+              imgpath: "../assets/images/notification.png",
+              name: "Notification",
+              ontap: () {},
+            ),
+          ],
         ),
-        DrawerListTile(
-          imgpath: "../assets/images/attendance.png",
-          name: "Attendance",
-          ontap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (BuildContext context) => Attendance(),
-              ),
-            );
-          },
-        ),
-        DrawerListTile(
-          imgpath: "../assets/images/classroom.png",
-          name: "Class work",
-          ontap: () {},
-        ),
-        DrawerListTile(
-          imgpath: "../assets/images/profile.png",
-          name: "Profile",
-          ontap: () {},
-        ),
-        DrawerListTile(
-          imgpath: "../assets/images/exam.png",
-          name: "Examination",
-          ontap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (BuildContext context) => ExamResult(),
-              ),
-            );
-          },
-        ),
-        DrawerListTile(
-          imgpath: "../assets/images/fee.png",
-          name: "Fees",
-          ontap: () {},
-        ),
-        DrawerListTile(
-          imgpath: "../assets/images/calendar.png",
-          name: "Time Table",
-          ontap: () {},
-        ),
-        DrawerListTile(
-          imgpath: "../assets/images/library.png",
-          name: "Library",
-          ontap: () {},
-        ),
-        DrawerListTile(
-          imgpath: "../assets/images/downloads.png",
-          name: "Downloads",
-          ontap: () {},
-        ),
-        DrawerListTile(
-          imgpath: "../assets/images/bus.png",
-          name: "Track ",
-          ontap: () {},
-        ),
-        DrawerListTile(
-          imgpath: "../assets/images/leave_apply.png",
-          name: "Leave apply",
-          ontap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (BuildContext context) => LeaveApply(),
-              ),
-            );
-          },
-        ),
-        DrawerListTile(
-          imgpath: "../assets/images/activity.png",
-          name: "Activity",
-          ontap: () {},
-        ),
-        DrawerListTile(
-          imgpath: "../assets/images/notification.png",
-          name: "Notification",
-          ontap: () {},
-        ),
-      ],
-    ),
       ),
     );
   }
