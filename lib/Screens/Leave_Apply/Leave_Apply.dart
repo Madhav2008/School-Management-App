b91e71bbbd86c26649e7fa37a4c7f7ac3b9514ca
arora <aroramadhav65@gmail.com>
2022-05-25 15:51:12 +0530

    Update

--- lib/Screens/Leave_Apply/Leave_Apply.dart
+++ lib/Screens/Leave_Apply/Leave_Apply.dart
@@ -50,7 +50,7 @@ class _LeaveApplyState extends State<LeaveApply>
     animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
       parent: animationController,
       curve: Curves.fastOutSlowIn,
-    ));
+    ),);
 
     delayedAnimation = Tween(begin: 1.0, end: 0.0).animate(
       CurvedAnimation(
