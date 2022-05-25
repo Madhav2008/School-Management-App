0573980e15df74c5d54541e15fcfd9bdb2682fb3
arora <aroramadhav65@gmail.com>
2022-05-25 17:29:36 +0530

    Update

--- lib/Screens/drawer.dart
+++ lib/Screens/drawer.dart
@@ -1,10 +1,3 @@
 import 'package:flutter/material.dart';
 
-class NavigationDrawerWidget extends StatelessWidget {
-  const NavigationDrawerWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    
-  }
-}
+stle
