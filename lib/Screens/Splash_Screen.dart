71a9942f65f9427f1103d7aa9d1006c69e311d26
arora <aroramadhav65@gmail.com>
2022-05-27 20:00:06 +0530

    Update

--- lib/Screens/Splash_Screen.dart
+++ lib/Screens/Splash_Screen.dart
@@ -43,7 +43,7 @@ class _SplashScreenState extends State<SplashScreen> {
                   fit: BoxFit.cover,
                 ),
               ),
-        child: Center(
+       Center(
           child: Image.asset(
             '../assets/images/splash.gif',
             width: MediaQuery.of(context).size.width,
