bd3bf1e67c173b5f1bead0b5a02aefd8951e6026
arora <aroramadhav65@gmail.com>
2022-05-27 22:37:19 +0530

    Update

--- lib/Widgets/AppBar.dart
+++ lib/Widgets/AppBar.dart
@@ -16,53 +16,54 @@ class CommonAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: 
-                  [Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    child: Opacity(
-                      opacity: 0.2,
-                      child: Image.asset(
-                        '../assets/images/back.jpg',
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-      AppBar(
-        title: Text(
-          "${title}",
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Opacity(
+            opacity: 0.2,
+            child: Image.asset(
+              '../assets/images/back.jpg',
+              fit: BoxFit.cover,
+            ),
           ),
         ),
-        iconTheme: IconThemeData(color: Colors.black),
-        // leading: menuenabled == true
-        //     ? IconButton(
-        //         color: Colors.black,
-        //         onPressed: () => ontap,
-        //         icon: Icon(
-        //           Icons.menu,
-        //         ),
-        //       )
-        //     : null,
-        actions: [
-          notificationenabled == true
-              ? InkWell(
-                  onTap: () {},
-                  child: Image.asset(
-                    "../assets/images/notification.png",
-                    width: 35,
+        AppBar(
+          title: Text(
+            "${title}",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          iconTheme: IconThemeData(color: Colors.black),
+          // leading: menuenabled == true
+          //     ? IconButton(
+          //         color: Colors.black,
+          //         onPressed: () => ontap,
+          //         icon: Icon(
+          //           Icons.menu,
+          //         ),
+          //       )
+          //     : null,
+          actions: [
+            notificationenabled == true
+                ? InkWell(
+                    onTap: () {},
+                    child: Image.asset(
+                      "../assets/images/notification.png",
+                      width: 35,
+                    ),
+                  )
+                : SizedBox(
+                    width: 1,
                   ),
-                )
-              : SizedBox(
-                  width: 1,
-                ),
-        ],
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-      ),],
+          ],
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+      ],
     );
   }
 
