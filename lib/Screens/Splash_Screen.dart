0f1bdb6b47ea05505ecd637f4185c767665948f6
arora <aroramadhav65@gmail.com>
2022-05-28 23:28:44 +0530

    Update

--- lib/Screens/Splash_Screen.dart
+++ lib/Screens/Splash_Screen.dart
@@ -39,7 +39,7 @@ class _SplashScreenState extends State<SplashScreen> {
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
             child: Opacity(
-              opacity: 0.2,
+              opacity: 0.3,
               child: Image.asset(
                 '../assets/images/back.jpg',
                 fit: BoxFit.cover,
