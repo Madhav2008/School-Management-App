d79c4bf8fbe65547bb1d001ff84de48153f4595f
arora <aroramadhav65@gmail.com>
2022-05-28 23:39:08 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -49,7 +49,7 @@ class _NavigationState extends State<Navigation> {
                 ),
                 Icon(Icons.add_box,
                     size: 30,
-                    color: (_page == 2) ? Colors.redAccent : Colors.white),
+                    color: (_page == 2) ? Colors.redAccent : Colors.white,),
                 Icon(Icons.search_sharp,
                     size: 30,
                     color: (_page == 3) ? Colors.redAccent : Colors.white),
