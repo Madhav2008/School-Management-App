dc36f0c71b0a849ce67ba42a041e241f5605a3f2
arora <aroramadhav65@gmail.com>
2022-05-28 23:39:24 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -59,7 +59,7 @@ class _NavigationState extends State<Navigation> {
                 ),
                 Icon(Icons.account_circle,
                     size: 30,
-                    color: (_page == 4) ? Colors.redAccent : Colors.white),
+                    color: (_page == 4) ? Colors.redAccent : Colors.white,),
               ],
               color: Color.fromRGBO(226, 55, 68, 0.60),
               buttonBackgroundColor: Colors.grey[50],
