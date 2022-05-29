edecf84c6f3d04f26344896bbc2265ceb415d0d4
arora <aroramadhav65@gmail.com>
2022-05-29 11:57:53 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -19,7 +19,7 @@ class _NavigationState extends State<Navigation> {
     SettingsScreen(),
     HomeScreen(),
   ];
-  int _page = 0;
+  int _page = 2;
   GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 
   @override
