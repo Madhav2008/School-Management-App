6a72470ca8455e3f11f4b5f192689702ea81145a
arora <aroramadhav65@gmail.com>
2022-05-29 12:58:42 +0530

    Update

--- lib/Screens/Profile_Screen.dart
+++ lib/Screens/Profile_Screen.dart
@@ -218,7 +218,7 @@ class _ProfileState extends State<Profile> {
                                 ],
                               ),
                               SizedBox(
-                                width: 150,
+                                width: 180,
                                 child: Padding(
                                   padding: const EdgeInsets.only(
                                     top: 5.0,
