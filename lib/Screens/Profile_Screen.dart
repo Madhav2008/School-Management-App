21439d2d541e2df953db284b2d57739f54d4c9e0
arora <aroramadhav65@gmail.com>
2022-05-29 13:04:29 +0530

    Update

--- lib/Screens/Profile_Screen.dart
+++ lib/Screens/Profile_Screen.dart
@@ -234,7 +234,7 @@ class _ProfileState extends State<Profile> {
                             ],
                           ),
                           SizedBox(
-                            width: 40,
+                            width: 20,
                           ),
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
