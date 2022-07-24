48b7e0967858530386c0cbd1bfca9bf15f19d56b
arora <aroramadhav65@gmail.com>
2022-07-24 11:25:40 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -118,9 +118,28 @@ class _HomeScreenState extends State<HomeScreen>
                   TextField(
                     // onChanged: (value) => _runFilter(value),
                     decoration: InputDecoration(
-                      labelText: 'Search',
-                      suffixIcon: Icon(Icons.search),
-                    ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.blueAccent,
+                            width: 0.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blue,
+                            width: 0.0,
+                          ),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.mail,
+                          color: Colors.white,
+                        ),
+                        hintText: 'Email',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                        fillColor: Colors.white,
+                        focusColor: Colors.white,),
                   ),
                   Padding(
                     padding: const EdgeInsets.fromLTRB(
