0fd48394fbec46dc4580cda9079536effd81b932
arora <aroramadhav65@gmail.com>
2022-07-24 11:43:24 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -120,7 +120,7 @@ class _HomeScreenState extends State<HomeScreen>
                       horizontal: 10.0,
                       vertical: 10.0,
                     ),
-                    child: Material(elevation: ,
+                    child: Material(elevation: 18,
                       child: TextField(
                         // onChanged: (value) => _runFilter(value),
                         decoration: InputDecoration(
