185a91f75b63cea24415116bfe79937ace7dba79
arora <aroramadhav65@gmail.com>
2022-07-24 11:36:42 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -116,7 +116,7 @@ class _HomeScreenState extends State<HomeScreen>
                 children: [
                   UserDetailCard(),
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: ),
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0),
                     child: TextField(
                       // onChanged: (value) => _runFilter(value),
                       decoration: InputDecoration(
