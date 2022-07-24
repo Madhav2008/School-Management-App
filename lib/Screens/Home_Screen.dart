3cb67c75ec790453caf29e7eb892f7b0179f8ef9
arora <aroramadhav65@gmail.com>
2022-07-24 11:33:41 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -121,7 +121,7 @@ class _HomeScreenState extends State<HomeScreen>
                       // onChanged: (value) => _runFilter(value),
                       decoration: InputDecoration(
                         enabledBorder: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderRadius: BorderRadius.all(Radius.circular()),
                           borderSide: const BorderSide(
                             color: Colors.blueAccent,
                             width: 0.0,
