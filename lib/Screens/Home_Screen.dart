ff8473585d16ffe549d4f2a82bba00f5fb37d88c
arora <aroramadhav65@gmail.com>
2022-07-24 11:46:27 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -130,7 +130,7 @@ class _HomeScreenState extends State<HomeScreen>
                             borderRadius: BorderRadius.all(
                               Radius.circular(100),
                             ),
-                            borderSide: BorderSide.none,
+                            // borderSide: BorderSide.none,
                           ),
                           prefixIcon: Icon(
                             Icons.search,
