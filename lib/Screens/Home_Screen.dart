bd9c56209b8ab8eeabf5e53e3428673281a5ef74
arora <aroramadhav65@gmail.com>
2022-07-24 12:16:10 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -138,7 +138,7 @@ class _HomeScreenState extends State<HomeScreen>
                           ),
                           hintText: 'Search',
                           hintStyle: TextStyle(
-                            color: Colors.grey,
+                            color: Colors.grey,fontSize
                           ),
                           fillColor: Colors.white,
                           filled: true,
