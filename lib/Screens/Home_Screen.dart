0ad652291c5721b2b757279b29e18859e945ace9
arora <aroramadhav65@gmail.com>
2022-07-24 11:41:02 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -144,7 +144,7 @@ class _HomeScreenState extends State<HomeScreen>
                         hintStyle: TextStyle(
                           color: Colors.black,
                         ),
-                        fillColor: Colors.black,
+                        fillColor: Colors.white,
                         filled: true,
                         focusColor: Colors.black,
                       ),
