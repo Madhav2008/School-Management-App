a134b45711d18bc751fea296d0dc5b6629d46849
arora <aroramadhav65@gmail.com>
2022-07-26 19:53:44 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -107,13 +107,7 @@ class _HomeScreenState extends State<HomeScreen>
   final List<Map<String, dynamic>> _allDetails = [
     {
       "name": "Madhav Arora",
-      "message": "Hey Flutter, You are so amazing !",
-      "time": "15:30",
-      "avatarUrl":
-          "https://avatars.githubusercontent.com/u/72864817?s=400&u=2f8a4bd2f1f03f4f6ad73c61abfc5770afd1e135&v=4",
-      "about": "I overthink therefore I overam",
-      "phoneno": '9999348666',
-      "id": 1,
+      
     },
   ];
   void _runFilter(String enteredKeyword) {
